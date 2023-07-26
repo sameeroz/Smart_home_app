f90a0f5b456b780b62762ac11e60931007fd3b8a
Sameer Anwar <gdrboy11@gmail.com>
2023-07-26 05:04:51 +0300

    oranizing the project

--- app/src/androidTest/java/com/sameer/smart_home/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sameer/smart_home/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.sameer.arduinocontroller", appContext.getPackageName());
+        assertEquals("smart_home", appContext.getPackageName());
     }
 }
